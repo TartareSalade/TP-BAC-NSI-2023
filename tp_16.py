f0c117325378c96fb6a62593a9550728f5edf7e6
TartareSalade <lyzoriaserveur@gmail.com>
2023-02-23 18:17:52 +0100

    Update tp_16.py

--- tp_16.py
+++ tp_16.py
@@ -1,1 +1,51 @@
-print("test")
+def recherche_indices_classement(elt, tab):
+    liste_1 = []
+    liste_2 = []
+    liste_3 = []
+    for i in range (len(tab)):
+        if tab[i] < elt:
+            liste_1.append(i)
+        if tab[i] == elt:
+            liste_2.append(i)
+        if tab[i] > elt:
+            liste_3.append(i)
+    return liste_1, liste_2, liste_3
+
+
+
+print(recherche_indices_classement(3, [1, 3, 4, 2, 4, 6, 3, 0]))
+print(recherche_indices_classement(3, [1, 4, 2, 4, 6, 0]))
+print(recherche_indices_classement(3, [1, 1, 1, 1]))
+print(recherche_indices_classement(3, []))
+
+
+resultats = {'Dupont': {
+                           'DS1': [15.5, 4],
+                           'DM1': [14.5, 1],
+                           'DS2': [13, 4],
+                           'PROJET1': [16, 3],
+                           'DS3': [14, 4]
+                       },
+             'Durand': {
+                           'DS1': [6 , 4],
+                           'DM1': [14.5, 1],
+                           'DS2': [8, 4],
+                           'PROJET1': [9, 3],
+                           'IE1': [7, 2],
+                           'DS3': [8, 4],
+                           'DS4':[15, 4]
+                       }
+            }
+
+def moyenne(nom, dico_result):
+    if nom in dico_result:
+        notes = dico_result[nom]
+        total_points = 0
+        total_coefficients = 0
+        for valeurs  in notes.values():
+            note, coefficient = valeurs
+            total_points = total_points + note * coefficient
+            total_coefficients =  total_coefficients + coefficient
+        return round( total_points / total_coefficients, 1 )
+    else:
+        return -1
